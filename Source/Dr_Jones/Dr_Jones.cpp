58ddcf37a9ba4a2209b620c54f69c25d714a4686
Kamil Bochenski <kbochenski24@gmail.com>
2022-10-16 22:46:29 +0200

    Initialize Unreal Engine 5.03 files

---
+++ Source/Dr_Jones/Dr_Jones.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Dr_Jones.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Dr_Jones, "Dr_Jones" );
---
+++ Source/Dr_Jones/Dr_JonesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Dr_JonesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DR_JONES_API ADr_JonesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
