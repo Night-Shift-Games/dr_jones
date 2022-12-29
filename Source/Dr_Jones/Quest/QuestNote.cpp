5518663317d4daf386de1802cb820699bf8a1db1
Jacek Rogal <burnertski@gmail.com>
2022-12-29 22:08:52 +0100

    Moved Quest note actor to C++

--- Source/Dr_Jones/Quest/QuestNote.cpp
+++ Source/Dr_Jones/Quest/QuestNote.cpp
@@ -2,11 +2,30 @@
 
 
 #include "QuestNote.h"
+#include "Quest.h"
+
+FName AQuestNote::NoteMeshComponentName = TEXT("NoteMesh");
 
 AQuestNote::AQuestNote()
 {
 	PrimaryActorTick.bCanEverTick = false;
 	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	NoteMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(AQuestNote::NoteMeshComponentName);
+	RootComponent = NoteMeshComponent;
+}
+
+void AQuestNote::Initialize(UQuestData& QuestDataObject)
+{
+	QuestData = &QuestDataObject;
+}
+
+void AQuestNote::OnConstruction(const FTransform& Transform)
+{
+	check(NoteMeshComponent);
+
+	UStaticMesh* Mesh = QuestData ? QuestData->QuestNoteData.NoteMesh : nullptr;
+	NoteMeshComponent->SetStaticMesh(Mesh);
 }
 
 void AQuestNote::Interact(APawn* Indicator)
--- Source/Dr_Jones/Quest/QuestNote.h
+++ Source/Dr_Jones/Quest/QuestNote.h
@@ -4,33 +4,44 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Dr_Jones/Interactive/InteractiveObject.h"
+#include "Interactive/InteractiveObject.h"
 #include "QuestNote.generated.h"
 
 class UStaticMesh;
+class UQuestData;
 
 
-UCLASS(Blueprintable, BlueprintType, Category = "Quest System")
+UCLASS(Blueprintable, BlueprintType, Category="Quest System")
 class DR_JONES_API AQuestNote : public AActor, public IInteractiveObject
 {
 	GENERATED_BODY()
 
 public:
+	static FName NoteMeshComponentName;
+
 	AQuestNote();
+
+	/** Call before spawn to set the Quest data*/
+	void Initialize(UQuestData& QuestDataObject);
+
+	virtual void OnConstruction(const FTransform& Transform) override;
 
 	// IInteractiveObject overrides
 
 	virtual void Interact(APawn* Indicator) override;
 
-	UFUNCTION(BlueprintCallable, Category = "Interactive")
+	UFUNCTION(BlueprintCallable, Category="Interactive")
 	virtual FString GetInteractSentence() override;
 
 	// End of IInteractiveObject overrides
 
-	UFUNCTION(BlueprintImplementableEvent, Category = "Interactive", meta=(DisplayName="On Interact"))
+	UFUNCTION(BlueprintImplementableEvent, Category="Interactive", meta=(DisplayName="On Interact"))
 	void K2_OnInteract(APawn* Indicator);
 
 private:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest System", meta = (AllowPrivateAccess = true))
-	UStaticMesh* NoteMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true))
+	TObjectPtr<UStaticMeshComponent> NoteMeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true, ExposeOnSpawn=true))
+	TObjectPtr<UQuestData> QuestData;
 };
