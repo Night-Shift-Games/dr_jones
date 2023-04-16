6a8b6f7a05bb74230edf14c1246e3f865b9bdc8d
Burnert <burnertski@gmail.com>
2023-04-16 04:58:51 +0200

    Character Animation structs in Cpp
    + Added structs with animation stuff with montages and blend spaces to set per character and per tool

--- Source/Dr_Jones/Animation/CharacterAnimationComponent.cpp
+++ Source/Dr_Jones/Animation/CharacterAnimationComponent.cpp
@@ -10,7 +10,7 @@ UCharacterAnimationComponent::UCharacterAnimationComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	bWantsInitializeComponent = true;
 }
 
 
@@ -23,6 +23,20 @@ void UCharacterAnimationComponent::BeginPlay()
 	
 }
 
+void UCharacterAnimationComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	const AActor* Owner = GetOwner();
+	checkf(Owner, TEXT("Animation Component cannot have no owner."));
+
+	CharacterMeshComponent = Owner->FindComponentByClass<USkeletalMeshComponent>();
+	if (!CharacterMeshComponent)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Skeletal Mesh Component not found in the owner of Character Animation Component."));
+	}
+}
+
 
 // Called every frame
 void UCharacterAnimationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -30,5 +44,27 @@ void UCharacterAnimationComponent::TickComponent(float DeltaTime, ELevelTick Tic
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+const FCharacterToolAnimations& UCharacterAnimationComponent::FindAnimationsForTool(FName Tool, bool& bOutFound) const
+{
+	if (const FCharacterToolAnimations* Animations = CharacterToolAnimations.Find(Tool))
+	{
+		bOutFound = true;
+		return *Animations;
+	}
+
+	bOutFound = false;
+	static FCharacterToolAnimations NullAnimations {};
+	return NullAnimations;
+}
+
+UAnimMontage* UCharacterAnimationComponent::FindToolMontage(FName Tool, FName Montage) const
+{
+	if (const FCharacterToolAnimations* Animations = CharacterToolAnimations.Find(Tool))
+	{
+		return Animations->Montages.FindRef(Montage);
+	}
+	return nullptr;
 }
 
--- Source/Dr_Jones/Animation/CharacterAnimationComponent.h
+++ Source/Dr_Jones/Animation/CharacterAnimationComponent.h
@@ -7,12 +7,41 @@
 #include "CharacterAnimationComponent.generated.h"
 
 
+class ATool;
+
+
+USTRUCT(BlueprintType, Category = "Animation")
+struct FCharacterAnimations
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	TObjectPtr<UAnimSequence> Idle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	TObjectPtr<UBlendSpace> Movement;
+};
+
+
+USTRUCT(BlueprintType, Category = "Animation")
+struct FCharacterToolAnimations
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	TObjectPtr<UAnimSequence> Idle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	TMap<FName, TObjectPtr<UAnimMontage>> Montages;
+};
+
+
 UCLASS( Blueprintable, BlueprintType, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class DR_JONES_API UCharacterAnimationComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UCharacterAnimationComponent();
 
@@ -20,9 +49,25 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
+	virtual void InitializeComponent() override;
+
+public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	const FCharacterToolAnimations& FindAnimationsForTool(FName Tool, bool& bOutFound) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	UAnimMontage* FindToolMontage(FName Tool, FName Montage) const;
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = "Animation", meta = (AllowPrivateAccess = true))
+	TObjectPtr<USkeletalMeshComponent> CharacterMeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation", meta = (AllowPrivateAccess = true))
+	FCharacterAnimations CharacterAnimations;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation", meta = (AllowPrivateAccess = true))
+	TMap<FName, FCharacterToolAnimations> CharacterToolAnimations;
 };
