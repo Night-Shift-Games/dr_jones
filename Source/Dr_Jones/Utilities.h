25a2a3b74bd1422bc471414ddeebeb4c64c65ace
Kamil Bocheński <kbochenski24@gmail.com>
2023-04-22 14:30:05 +0200

    Add wrap template for TSet

--- Source/Dr_Jones/Utilities.h
+++ Source/Dr_Jones/Utilities.h
@@ -3,8 +3,15 @@
 namespace Utilities
 {
 	uint32 WrapIndexToSize(int64 Index, uint32 Size);
+
 	template <typename T>
 	uint32 WrapIndexToArray(int64 Index, TArray<T> Array)
+	{
+		return WrapIndexToSize(Index, Array.Num());
+	}
+
+	template <typename T>
+	uint32 WrapIndexToArray(int64 Index, TSet<T> Array)
 	{
 		return WrapIndexToSize(Index, Array.Num());
 	}
