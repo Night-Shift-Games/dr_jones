125280d2c677085332f53fb6e9156999cf622764
Kamil Bocheński <kbochenski24@gmail.com>
2023-04-22 19:43:44 +0200

    Add widget manager

---
+++ Source/Dr_Jones/Player/WidgetManager.cpp
@@ -0,0 +1,23 @@
+#include "WidgetManager.h"
+
+void UWidgetManager::AddWidget(const TSubclassOf<UUserWidget> WidgetClass)
+{
+	UUserWidget* NewWidget = CreateWidget(GetWorld(), WidgetClass);
+	Widgets.Add(WidgetClass, NewWidget);
+}
+
+void UWidgetManager::ShowWidget(const TSubclassOf<UUserWidget> Widget)
+{
+	if (UUserWidget* WidgetToShow = Widgets.Find(Widget)->Get(); !WidgetToShow->IsInViewport())
+	{
+		WidgetToShow->AddToViewport();
+	}
+}
+
+void UWidgetManager::HideWidget(const TSubclassOf<UUserWidget> Widget)
+{
+	if (UUserWidget* WidgetToHide = Widgets.Find(Widget)->Get(); WidgetToHide->IsInViewport())
+	{
+		WidgetToHide->RemoveFromViewport();
+	}
+}
---
+++ Source/Dr_Jones/Player/WidgetManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+UCLASS()
+class UWidgetManager : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	void AddWidget(const TSubclassOf<UUserWidget> WidgetClass);
+	void ShowWidget(const TSubclassOf<UUserWidget> Widget);
+	void HideWidget(const TSubclassOf<UUserWidget> Widget);
+	void RemoveWidget(const TSubclassOf<UUserWidget> Widget);
+	void UpdateWidget(const TSubclassOf<UUserWidget> Widget);
+public:
+	UPROPERTY()
+	TMap<TSubclassOf<UUserWidget>, TObjectPtr<UUserWidget>> Widgets;
+};
