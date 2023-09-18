6865fcadbab0d91545b5a4ab8b8ec0a7f82ef9a8
Burnert <burnertski@gmail.com>
2023-09-18 04:11:09 +0200

    Quest System 2.0 part 2

--- Source/Dr_Jones/Quest/QuestSystem.cpp
+++ Source/Dr_Jones/Quest/QuestSystem.cpp
@@ -9,9 +9,71 @@ TOptional<FQuestHandle> UQuest::GetHandle() const
 	return HandleInRegistry;
 }
 
+UWorld* UQuest::GetWorld() const
+{
+	const UObject* Outer = GetOuter();
+	if (!Outer)
+	{
+		return nullptr;
+	}
+
+	if (HasAnyFlags(RF_ClassDefaultObject) || Outer->HasAnyFlags(RF_BeginDestroyed) || Outer->IsUnreachable())
+	{
+		return nullptr;
+	}
+
+	return Outer->GetWorld();
+}
+
+void UQuest::InitializePending()
+{
+	OnInitializePending();
+}
+
+bool UQuest::IsCompleted() const
+{
+	return bCompleted;
+}
+
+const FQuestDescription& UQuest::GetDescription() const
+{
+	return QuestDescription;
+}
+
 bool UQuest::IsRegistered() const
 {
 	return HandleInRegistry.IsSet();
+}
+
+void UQuest::MarkAsCompleted()
+{
+	check(IsRegistered());
+
+	if (bCompleted)
+	{
+		UE_LOG(LogDrJones, Warning, TEXT("The Quest %s (%i: %s) is already completed."),
+			*QuestDescription.DisplayName.ToString(), HandleInRegistry.GetValue().GetKey(), *GetName());
+		return;
+	}
+
+	bCompleted = true;
+	(void)NotifyCompletedDelegate.ExecuteIfBound();
+
+	QuestSystemUtils::LogDebugIfEnabled(TEXT("Quest \"%s\" (%s) has been marked as complete."),
+		*QuestDescription.DisplayName.ToString(), *GetName());
+}
+
+void UQuestSystemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	for (const FQuestHandle Handle : PendingQuests)
+	{
+		UQuest* Quest = QuestRegistry.FindChecked(Handle);
+		checkf(Quest, TEXT("Null quest found during BeginPlay pending quests iteration."));
+
+		InitializePendingQuest(*Quest);
+	}
 }
 
 UQuest* UQuestSystemComponent::AddQuest(const FQuestDescription& QuestDescription)
@@ -25,13 +87,23 @@ UQuest* UQuestSystemComponent::AddQuest(const FQuestDescription& QuestDescriptio
 	UQuest* QuestObject = NewObject<UQuest>(this, QuestDescription.QuestClass);
 	check(QuestObject);
 
-	QuestObject->HandleInRegistry = RegisterQuest(*QuestObject);
+	const FQuestHandle QuestHandle = RegisterQuest(*QuestObject);
+	QuestObject->HandleInRegistry = QuestHandle;
+	QuestObject->QuestDescription = QuestDescription;
+
+	PendingQuests.Add(QuestHandle);
+	InitializePendingQuest(*QuestObject);
+
+	QuestSystemUtils::LogDebugIfEnabled(TEXT("Quest \"%s\" (%s) has been added to the System."),
+		*QuestDescription.DisplayName.ToString(), *QuestObject->GetName());
 
 	return QuestObject;
 }
 
 FQuestHandle UQuestSystemComponent::RegisterQuest(UQuest& Quest)
 {
+	check(Quest.GetWorld());
+
 	if (Quest.IsRegistered())
 	{
 		UE_LOG(LogDrJones, Warning, TEXT("Cannot register an already registered quest."));
@@ -40,5 +112,33 @@ FQuestHandle UQuestSystemComponent::RegisterQuest(UQuest& Quest)
 
 	FQuestHandle QuestHandle = FQuestHandle::New();
 	QuestRegistry.Emplace(QuestHandle, &Quest);
+
+	QuestSystemUtils::LogDebugIfEnabled(TEXT("Quest \"%s\" (%s) has been registered."),
+		*Quest.QuestDescription.DisplayName.ToString(), *Quest.GetName());
+
 	return QuestHandle;
+}
+
+void UQuestSystemComponent::InitializePendingQuest(UQuest& Quest)
+{
+	checkf(Quest.IsRegistered(), TEXT("Cannot initialize a not registered quest."));
+	check(Quest.GetWorld());
+
+	const FQuestHandle Handle = Quest.HandleInRegistry.GetValue();
+	Quest.NotifyCompletedDelegate.BindLambda([this, Handle]
+	{
+		PendingQuests.Remove(Handle);
+
+		if (QuestSystemUtils::IsDebugEnabled())
+		{
+			const UQuest* FoundQuest = QuestRegistry.FindChecked(Handle);
+			check(FoundQuest);
+			QuestSystemUtils::LogDebugIfEnabled(TEXT("Quest \"%s\" (%s) has been removed from pending quests array."),
+				*FoundQuest->QuestDescription.DisplayName.ToString(), *FoundQuest->GetName());
+		}
+	});
+	Quest.InitializePending();
+
+	QuestSystemUtils::LogDebugIfEnabled(TEXT("Pending Quest \"%s\" (%s) has been initialized."),
+		*Quest.QuestDescription.DisplayName.ToString(), *Quest.GetName());
 }
--- Source/Dr_Jones/Quest/QuestSystem.h
+++ Source/Dr_Jones/Quest/QuestSystem.h
@@ -3,11 +3,19 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Dr_Jones.h"
 #include "Components/ActorComponent.h"
 
 #include "QuestSystem.generated.h"
 
 class UQuest;
+
+static TAutoConsoleVariable CVarQuestSystemDebug(
+	TEXT("NS.QuestSystem.Debug"),
+	false,
+	TEXT("Enable Quest System debug logging."),
+	ECVF_Cheat
+);
 
 USTRUCT(BlueprintType)
 struct FQuestHandle
@@ -36,6 +44,11 @@ struct FQuestHandle
 	bool IsValid() const
 	{
 		return Key != InvalidKey;
+	}
+
+	KeyType GetKey() const
+	{
+		return Key;
 	}
 
 	FQuestHandle() = default;
@@ -74,20 +87,42 @@ struct FQuestDescription
 	TSubclassOf<UQuest> QuestClass;
 };
 
-UCLASS(Blueprintable)
+UCLASS(Blueprintable, Abstract)
 class DR_JONES_API UQuest : public UObject
 {
 	GENERATED_BODY()
 
 public:
+	DECLARE_DELEGATE(FNotifyCompletedDelegate);
+
+	virtual UWorld* GetWorld() const override;
+
+	void InitializePending();
+
+	const FQuestDescription& GetDescription() const;
+	bool IsCompleted() const;
+
 	// Handle can be invalid if the quest has not been registered yet.
 	TOptional<FQuestHandle> GetHandle() const;
 
 	UFUNCTION(BlueprintPure, Category = "Quest")
 	bool IsRegistered() const;
 
+	UFUNCTION(BlueprintCallable, Category = "Quest")
+	void MarkAsCompleted();
+
+protected:
+	// Called when a pending task should initialize its properties/events.
+	// When a new quest is added or, for an existing quest, when the level is started.
+	UFUNCTION(BlueprintImplementableEvent, Category = "Quest")
+	void OnInitializePending();
+
 private:
 	TOptional<FQuestHandle> HandleInRegistry;
+	FNotifyCompletedDelegate NotifyCompletedDelegate;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Quest", meta = (AllowPrivateAccess = true))
+	FQuestDescription QuestDescription;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Quest", meta = (AllowPrivateAccess = true))
 	uint8 bCompleted : 1;
@@ -100,13 +135,50 @@ class DR_JONES_API UQuestSystemComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+public:
+	virtual void BeginPlay() override;
+
 	UFUNCTION(BlueprintCallable, Category = "Quest")
 	UPARAM(DisplayName = "Quest Object") UQuest* AddQuest(const FQuestDescription& QuestDescription);
 
-public:
+protected:
 	FQuestHandle RegisterQuest(UQuest& Quest);
+	void InitializePendingQuest(UQuest& Quest);
 
 private:
-	UPROPERTY()
+	UPROPERTY(SaveGame)
+	TArray<FQuestHandle> PendingQuests;
+
+	UPROPERTY(SaveGame)
 	TMap<FQuestHandle, UQuest*> QuestRegistry;
 };
+
+namespace QuestSystemUtils
+{
+#if !NO_LOGGING
+	static bool IsDebugEnabled() { return CVarQuestSystemDebug.GetValueOnGameThread(); }
+
+	template<int32 Length, typename... Types>
+	static void LogDebug(const TCHAR(&Format)[Length], Types&&... Args)
+	{
+		check(GEngine);
+		const FString Message = FString::Printf(Format, Forward<Types>(Args)...);
+		GEngine->AddOnScreenDebugMessage(-1, 4.0f, FColor::Turquoise, Message);
+		// Yep. TEXT("%s"). I don't care...
+		UE_LOG(LogDrJones, Log, TEXT("%s"), *Message);
+	}
+
+	template<int32 Length, typename... Types>
+	static void LogDebugIfEnabled(const TCHAR(&Format)[Length], Types&&... Args)
+	{
+		if (IsDebugEnabled())
+		{
+			LogDebug(Format, Forward<Types>(Args)...);
+		}
+	}
+#else
+	static constexpr bool IsQuestSystemDebugEnabled() { return false; }
+	template<int32 Length, typename... Types> static void LogDebug(const FString& Message) { }
+	template<int32 Length, typename... Types> static void LogDebugIfEnabled(const FString& Message) { }
+#endif
+}
