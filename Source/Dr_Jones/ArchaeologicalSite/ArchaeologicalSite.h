ece592a6bf7ab901e0eda110f65fcabd6cfc6ef8
Kamil Bocheński <kbochenski24@gmail.com>
2023-09-20 01:07:57 +0200

    Check(Sanity) => Critical Error

--- Source/Dr_Jones/ArchaeologicalSite/ArchaeologicalSite.h
+++ Source/Dr_Jones/ArchaeologicalSite/ArchaeologicalSite.h
@@ -3,10 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "ArchaeologicalSite/Chunk.h"
 #include "GameFramework/Actor.h"
 
 #include "ArchaeologicalSite.generated.h"
+
+struct FChunk;
 
 UCLASS()
 class DR_JONES_API AArchaeologicalSite : public AActor
@@ -16,18 +17,4 @@ class DR_JONES_API AArchaeologicalSite : public AActor
 public:
 	AArchaeologicalSite();
 	virtual void Tick(float DeltaSeconds) override;
-
-	UFUNCTION(BlueprintCallable)
-	void TestDig(FVector Direction, FTransform SphereOrigin);
-
-	UFUNCTION(BlueprintCallable)
-	void SampleChunk(FVector Location);
-	
-	FVector CalculateSphereDeform(const FVector& VertexPosition, const FVector& SphereOrigin, const float SphereRadius, const FVector& DigDirection) const;
-	FMasterChunk& GetChunkAtLocation(const FVector& Location);
-	void DrawChunkDebug();
-
-public:
-	TMap<FIntVector3, TSharedPtr<FMasterChunk>> Chunks;
-
 };
--- Source/Dr_Jones/ArchaeologicalSite/Chunk.h
+++ Source/Dr_Jones/ArchaeologicalSite/Chunk.h
@@ -1,50 +1,7 @@
 #pragma once
+#include "ArchaeologicalSite.h"
 #include "Components/DynamicMeshComponent.h"
-
-struct FSubChunk;
 
 struct FChunk
 {
-	FChunk(const FVector& Location, int NewResolution);
-
-	const FVector& GetWorldLocation() const { return WorldLocation; }
-	const FVector& GetRelativeLocation() const { return RelativeLocation; }
-	
-	double Resolution = 0;
-	FColor Color = FColor::Red;
-
-protected:
-	FVector WorldLocation;
-	FVector RelativeLocation;
-};
-
-struct FMasterChunk : public FChunk
-{
-	FMasterChunk(const FVector& NewLocation, int NewResolution);
-	FSubChunk* GetSubChunkAtLocation(const FVector& Location);
-	void RefreshMesh();
-	bool IsSurface(FSubChunk* Chunk);
-
-	void CreateSurface();
-	
-protected:
-	int8 FindDirectionSign(float Lenght);
-	
-public:
-	TMap<FIntVector3, TSharedPtr<FSubChunk>> SubChunks;
-	TMap<FIntVector3, TSharedPtr<FSubChunk>> SurfaceChunks;
-	
-protected:
-	TStrongObjectPtr<UDynamicMeshComponent> DynamicMeshComponent;
-};
-
-struct FSubChunk : public FChunk
-{
-	FSubChunk(FMasterChunk& NewOwner, const FVector& NewLocation, int NewResolution);
-	FMasterChunk& Owner;
-
-	FSubChunk* FindNeighbor(const FIntVector& NeighborOrientation) const;
-	
-	bool bSolid = true;
-	bool bSurface = false;
 };
