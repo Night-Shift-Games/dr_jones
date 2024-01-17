fb5a9c12b20d48c914429ead4155d79bcfb49aeb
Kamil Bocheński <kbochenski24@gmail.com>
2024-01-17 01:57:04 +0100

    Fix item in hand collision channel

--- Source/Dr_Jones/Items/Item.cpp
+++ Source/Dr_Jones/Items/Item.cpp
@@ -23,6 +23,7 @@ void AItem::SetAttachmentPhysics()
 	if (UMeshComponent* Mesh = GetMeshComponent())
 	{
 		Mesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+		Mesh->SetCollisionResponseToChannel(ECC_Visibility, ECR_Ignore);
 		Mesh->SetSimulatePhysics(false);
 	}
 }
@@ -33,6 +34,7 @@ void AItem::SetGroundPhysics()
 	if (UMeshComponent* Mesh = GetMeshComponent())
 	{
 		Mesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Block);
+		Mesh->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
 		Mesh->SetSimulatePhysics(false);
 	}
 }
