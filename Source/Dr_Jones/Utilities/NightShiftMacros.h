2d70b4705b88c102a7e3ffbdd2577700388978cf
Kamil Bocheński <kbochenski24@gmail.com>
2024-05-25 00:18:08 +0200

    Sneaky

---
+++ Source/Dr_Jones/Utilities/NightShiftMacros.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#ifndef NS_EARLY
+	#define B_EARLY(exp) \
+	{\
+		if (exp)\
+		{\
+			return;\
+		}\
+	}
+#endif
+
+#ifndef NS_EARLY_R
+	#define B_EARLY_R(exp, retval) \
+	{\
+		if (exp)\
+		{\
+			return retval;\
+		}\
+	}
+#endif
