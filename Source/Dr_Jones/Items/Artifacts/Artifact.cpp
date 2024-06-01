72b5c6f64cc7ffbae42b4d5a1d992895e6c3c3f0
Burnert <burnertski@gmail.com>
2024-06-01 19:17:41 +0200

    Comment fix

--- Source/Dr_Jones/Items/Artifacts/Artifact.cpp
+++ Source/Dr_Jones/Items/Artifacts/Artifact.cpp
@@ -602,7 +602,7 @@ void UArtifactIdentificationMode::Tick(float DeltaSeconds)
 			return;
 		}
 
-		// right vector because blender snaps things   is Y and we're setting up the sockets using snap's system
+		// right vector because blender snaps things with Y as front, and we're setting up the sockets using snap's system
 		const double Dot = FVector::DotProduct(-Sphere->GetRightVector(), WorldDirection);
 		if (Dot <= 0)
 		{
