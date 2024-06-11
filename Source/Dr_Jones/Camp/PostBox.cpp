40291c56997bdd69c8da99aab6691831863f6f30
Burnert <burnertski@gmail.com>
2024-06-12 01:44:20 +0200

    Letter events & post box

---
+++ Source/Dr_Jones/Camp/PostBox.cpp
@@ -0,0 +1,13 @@
+﻿// Property of Night Shift Games, all rights reserved.
+
+#include "PostBox.h"
+
+#include "Interaction/InteractableComponent.h"
+
+APostBox::APostBox()
+{
+	PostBoxMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PostBoxMesh"));
+	RootComponent = PostBoxMesh;
+
+	InteractableComponent->SetupAttachment(PostBoxMesh);
+}
---
+++ Source/Dr_Jones/Camp/PostBox.h
@@ -0,0 +1,20 @@
+﻿// Property of Night Shift Games, all rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CampEntity.h"
+
+#include "PostBox.generated.h"
+
+UCLASS(Blueprintable)
+class DR_JONES_API APostBox : public ACampEntity
+{
+	GENERATED_BODY()
+
+public:
+	APostBox();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> PostBoxMesh;
+};
