19fd0481824d9f46f947d6e044ffc441126ddc02
Burnert <burnertski@gmail.com>
2024-06-14 02:00:10 +0200

    Added watch item

---
+++ Source/Dr_Jones/Items/Tools/Watch.cpp
@@ -0,0 +1,39 @@
+﻿// Property of Night Shift Games, all rights reserved.
+
+#include "Watch.h"
+
+#include "Interaction/InteractableComponent.h"
+#include "World/Clock.h"
+
+AWatch::AWatch()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	WatchMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WatchMesh"));
+	InteractableComponent->SetupAttachment(WatchMesh);
+}
+
+void AWatch::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ClockPrivate = GetGameInstance()->GetSubsystem<UClock>();
+	ClockPrivate->OnClockTick.AddUniqueDynamic(this, &AWatch::OnClockTick);
+}
+
+void AWatch::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	const FTimespan& TimePerSecond = ClockPrivate->TimeToApplyPerSecond;
+	CurrentHourNormalized += TimePerSecond.GetHours() * DeltaSeconds / 12.0f;
+	CurrentMinuteNormalized += (TimePerSecond.GetMinutes() + TimePerSecond.GetSeconds() / 60.0f) * DeltaSeconds / 60.0f;
+
+	AdjustClockHands(CurrentHourNormalized, CurrentMinuteNormalized);
+}
+
+void AWatch::OnClockTick(FDateTime CurrentDate)
+{
+	CurrentHourNormalized = CurrentDate.GetHour12() / 12.0f + CurrentDate.GetMinute() / (60.0f * 12.0f);
+	CurrentMinuteNormalized = CurrentDate.GetMinute() / 60.0f + CurrentDate.GetSecond() / (60.0f * 60.0f);
+}
---
+++ Source/Dr_Jones/Items/Tools/Watch.h
@@ -0,0 +1,38 @@
+﻿// Property of Night Shift Games, all rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tool.h"
+
+#include "Watch.generated.h"
+
+class UClock;
+
+UCLASS(Blueprintable)
+class DR_JONES_API AWatch : public ATool
+{
+	GENERATED_BODY()
+
+public:
+	AWatch();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	UFUNCTION()
+	void OnClockTick(FDateTime CurrentDate);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void AdjustClockHands(float HourNormalized, float MinuteNormalized);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> WatchMesh;
+
+private:
+	UPROPERTY(Transient)
+	TObjectPtr<UClock> ClockPrivate;
+
+	float CurrentHourNormalized = 0.0f;
+	float CurrentMinuteNormalized = 0.0f;
+};
