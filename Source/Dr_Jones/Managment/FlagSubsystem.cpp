da72e42c97092b32460d30180fdc19f5ec961666
Kamil Bocheński <kbochenski24@gmail.com>
2024-06-19 18:38:53 +0200

    Flagz

---
+++ Source/Dr_Jones/Managment/FlagSubsystem.cpp
@@ -0,0 +1,16 @@
+﻿#include "FlagSubsystem.h"
+
+void UFlagSubsystem::SetFlag(FName FlagID)
+{
+	if (IsFlagSet(FlagID))
+	{
+		return;
+	}
+	GlobalFlags.Add(FlagID);
+	OnFlagAdded.Broadcast(FlagID);
+}
+
+bool UFlagSubsystem::IsFlagSet(FName FlagID) const
+{
+	return GlobalFlags.Contains(FlagID);
+}
---
+++ Source/Dr_Jones/Managment/FlagSubsystem.h
@@ -0,0 +1,31 @@
+﻿// Property of Night Shift Games, all rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+
+#include "FlagSubsystem.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGlobalFlagAdded, FName, FlagID);
+
+UCLASS()
+class DR_JONES_API UFlagSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetFlag(FName FlagID);
+
+	UFUNCTION(BlueprintPure)
+	bool IsFlagSet(FName FlagID) const;
+	
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Dr Jones")
+	FOnGlobalFlagAdded OnFlagAdded;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSet<FName> GlobalFlags;
+};
